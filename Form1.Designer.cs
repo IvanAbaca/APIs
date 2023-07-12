ca78e98a559ea6eea0cb71e806b05c99d228ccc4
IvanAbaca <ivanfabrizioabaca@gmail.com>
2023-07-12 00:22:06 -0300

    Cambios requeridos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pokedex = new PictureBox();
+            pokemon = new PictureBox();
+            left = new Button();
+            right = new Button();
+            name = new Label();
+            idLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)pokedex).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pokemon).BeginInit();
+            SuspendLayout();
+            // 
+            // pokedex
+            // 
+            pokedex.Image = Properties.Resources.plantilla;
+            pokedex.Location = new Point(-1, 0);
+            pokedex.Name = "pokedex";
+            pokedex.Size = new Size(786, 421);
+            pokedex.TabIndex = 0;
+            pokedex.TabStop = false;
+            pokedex.MouseDown += pokedex_MouseDown;
+            pokedex.MouseMove += pokedex_mouseMove;
+            // 
+            // pokemon
+            // 
+            pokemon.BackColor = Color.White;
+            pokemon.Location = new Point(82, 81);
+            pokemon.Name = "pokemon";
+            pokemon.Size = new Size(227, 120);
+            pokemon.SizeMode = PictureBoxSizeMode.Zoom;
+            pokemon.TabIndex = 1;
+            pokemon.TabStop = false;
+            // 
+            // left
+            // 
+            left.FlatStyle = FlatStyle.Flat;
+            left.ForeColor = Color.FromArgb(35, 35, 35);
+            left.Image = Properties.Resources.btn_izq;
+            left.Location = new Point(264, 344);
+            left.Name = "left";
+            left.Size = new Size(28, 30);
+            left.TabIndex = 2;
+            left.UseVisualStyleBackColor = true;
+            left.Click += left_Click;
+            // 
+            // right
+            // 
+            right.BackColor = Color.Transparent;
+            right.FlatStyle = FlatStyle.Flat;
+            right.ForeColor = Color.FromArgb(35, 35, 35);
+            right.Image = Properties.Resources.btn_der;
+            right.Location = new Point(324, 344);
+            right.Name = "right";
+            right.Size = new Size(28, 28);
+            right.TabIndex = 3;
+            right.UseVisualStyleBackColor = false;
+            right.Click += right_Click;
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.BackColor = Color.Transparent;
+            name.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            name.Location = new Point(510, 90);
+            name.Name = "name";
+            name.Size = new Size(233, 37);
+            name.TabIndex = 4;
+            name.Text = "Nombre Pokemon";
+            // 
+            // idLabel
+            // 
+            idLabel.AutoSize = true;
+            idLabel.BackColor = Color.White;
+            idLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            idLabel.Location = new Point(150, 366);
+            idLabel.Name = "idLabel";
+            idLabel.Size = new Size(53, 32);
+            idLabel.TabIndex = 5;
+            idLabel.Text = "000";
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(786, 420);
+            Controls.Add(idLabel);
+            Controls.Add(name);
+            Controls.Add(right);
+            Controls.Add(left);
+            Controls.Add(pokemon);
+            Controls.Add(pokedex);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Load += Form1_load;
+            ((System.ComponentModel.ISupportInitialize)pokedex).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pokemon).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pokedex;
+        private PictureBox pokemon;
+        private Button left;
+        private Button right;
+        private Label name;
+        private Label idLabel;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,11 @@
+using System.Data;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+
 namespace APIs
 {
     public partial class Form1 : Form
@@ -5,6 +13,66 @@ namespace APIs
         public Form1()
         {
             InitializeComponent();
+        }
+        private int idPokemon = 1;
+        private Point mouseLoc;
+        JObject data;
+        private void pokedex_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLoc = e.Location;
+        }
+
+        private void pokedex_mouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int dx = e.Location.X - mouseLoc.X;
+                int dy = e.Location.Y - mouseLoc.Y;
+                dx += this.Location.X;
+                dy += this.Location.Y;
+                this.Location = new Point(dx, dy);
+
+            }
+        }
+
+        private void cargaImagen()
+        {
+            string file = idPokemon.ToString("000");
+            idLabel.Text = file;
+            string pfile = $"images\\{file}.png";
+            pokemon.Image = Image.FromFile(pfile);
+            string id = idPokemon.ToString();
+            string nombre = data?["pokemon"]?[id]?["N"]?.Value<string>() ?? "";
+            name.TextAlign = ContentAlignment.MiddleCenter;
+            name.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(nombre);
+        }
+
+        private void Form1_load(object sender, EventArgs e)
+        {
+            name.Parent = pokedex;
+            name.BackColor = System.Drawing.Color.Transparent;
+
+            string text = File.ReadAllText("pokemon.json");
+            data = JObject.Parse(text);
+            cargaImagen();
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+            if (idPokemon >= 777)
+                idPokemon = 1;
+            else
+                idPokemon++;
+            cargaImagen();
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+            if (idPokemon <= 1)
+                idPokemon = 777;
+            else
+                idPokemon--;
+            cargaImagen();
         }
     }
 }
